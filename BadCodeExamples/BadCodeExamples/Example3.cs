7abcc2ce1878b58ea4c3fee8f2bc7a993c434e20
Vyacheslav <clava01.01@mail.ru>
2025-01-14 15:20:07 +0400

    Можно создать переменную для "0.1m * amount" и улучшить формулу при делении

--- BadCodeExamples/BadCodeExamples/Example3.cs
+++ BadCodeExamples/BadCodeExamples/Example3.cs
@@ -5,12 +5,13 @@
         public decimal Calculate(decimal amount, int type, int years)
         {
             decimal result = 0;
-            decimal disc = (years > 5) ? (decimal)5 / 100 : (decimal)years / 100;
+            decimal disc = (years > 5) ? (decimal)5 / 100 : (decimal)years / 100; // Рекомендуется сначала привести years к типу decimal перед делением
             if (type == 1)
             {
                 result = amount;
             }
             else if (type == 2)
+            //легче написать перменную и в ней хранить "0.1m * amount", чем каждый раз переписывать данное выражение 
             {
                 result = (amount - (0.1m * amount)) - disc * (amount - (0.1m * amount));
             }
